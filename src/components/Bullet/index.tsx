0ea5b9ae213e74a83d65e377bb5c1204f6b68ec6
cocosmos <mipam.guillot@crea-inseec.com>
2022-09-28 19:59:40 +0200

    🚧 Create components

---
+++ src/components/Bullet/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Wrapper } from "./styled";
+
+interface Props {}
+
+const Bullet = (props: Props) => {
+  return <Wrapper></Wrapper>;
+};
+
+export default Bullet;
---
+++ src/components/Player/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Wrapper } from './styled';
+
+interface Props {
+}
+
+const Player = (props: Props) => {
+    return <Wrapper></Wrapper>;
+};
+
+export default Player;
