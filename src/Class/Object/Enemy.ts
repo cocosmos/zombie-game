7e14d1868b747711e54b3b08afe123073d065b81
cocosmos <mipam.guillot@crea-inseec.com>
2022-10-04 19:14:10 +0200

    ✅ Finish enemy object

--- src/Class/Object/Enemy.ts
+++ src/Class/Object/Enemy.ts
@@ -1,15 +1,28 @@
 import { Coord } from "../../types/CommunType";
+import { move } from "../../utils/helper";
+import { Bullet } from "./Bullet";
 import { GameObject } from "./GameObject";
 
 export class Enemy extends GameObject {
-  constructor(position: Coord, speed: number) {
+  constructor(position: Coord, speed: number, degree: number) {
     super();
     this.position = position;
     this.speed = speed;
+    this.degree = degree;
   }
 
   update(): void {
-    this.position.x = this.position.x + this.speed;
-    this.position.y = this.position.y + this.speed;
+    this.position = move(this.position, this.degree, this.speed);
+
+    if (
+      this.position.x > 550 ||
+      this.position.y > 550 ||
+      this.position.x < -5 ||
+      this.position.y < -5
+    ) {
+      this.out = true;
+    } else {
+      this.out = false;
+    }
   }
 }
--- src/Class/Object/GameObject.ts
+++ src/Class/Object/GameObject.ts
@@ -4,8 +4,13 @@ import { Coord } from "../../types/CommunType";
 export class GameObject {
   position: Coord = { x: 0, y: 0 };
   velocity: Coord = { x: 0, y: 0 };
+  trajectory: Coord = { x: 0, y: 0 };
+  degree: number = 0;
+  out: boolean = false;
+
   speed: number = 0;
   id = nanoid();
   constructor() {}
   update() {}
+  stop() {}
 }
