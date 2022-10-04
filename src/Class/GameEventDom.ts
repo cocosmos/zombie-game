afffb4f240c8beffd3f5d997d103e03eddf55d7b
Cocosmos <mipam.guillot@crea-inseec.com>
2022-10-05 00:27:11 +0200

    🔥 Clean code

--- src/Class/GameEventDom.ts
+++ src/Class/GameEventDom.ts
@@ -1,4 +1,4 @@
-import { Coord } from "../types/CommunType";
+import { Coord, Size } from "../types/CommunType";
 import { move } from "../utils/helper";
 import { GameEngine, gameEngine } from "./GameEngine";
 import { Bullet } from "./Object/Bullet";
@@ -8,18 +8,19 @@ export class GameEventDom {
   angle: number = 0;
   clicked: number = 0;
   delta: Coord = { x: 0, y: 0 };
-  center = { w: window.innerWidth / 2, h: window.innerHeight / 2 };
+  gameSize: Size = { w: 0, h: 0 };
 
-  constructor() {}
+  constructor() {
+    this.gameSize = { w: window.innerWidth, h: window.innerHeight };
+  }
   init() {}
   destroy() {}
   mouseMove(event: any) {
     this.cursor.x = event.clientX;
     this.cursor.y = event.clientY;
-    this.center.w = 500 / 2;
-    this.center.h = 500 / 2;
-    this.delta.x = this.center.w - this.cursor.x;
-    this.delta.y = this.center.h - this.cursor.y;
+
+    this.delta.x = this.gameSize.w / 2 - this.cursor.x;
+    this.delta.y = this.gameSize.h / 2 - this.cursor.y;
 
     const radians = Math.atan2(this.delta.y, this.delta.x);
 
@@ -32,10 +33,15 @@ export class GameEventDom {
   onClick() {
     this.clicked = this.clicked + 1;
     gameEngine.fire(
-      new Bullet({ x: this.center.w, y: this.center.h }, this.angle, 10)
+      new Bullet(
+        { x: this.gameSize.w / 2, y: this.gameSize.h / 2 },
+        this.angle,
+        10
+      )
     );
   }
   onKeyDown(event: any) {
     console.log(event.key);
   }
 }
+export const gameEvent = new GameEventDom();
