18d4810ce8a747f441d143c2cd6bdb363b6dcba3
cocosmos <mipam.guillot@crea-inseec.com>
2022-10-04 19:13:48 +0200

    ✅ Finish bullet object

--- src/Class/Object/Bullet.ts
+++ src/Class/Object/Bullet.ts
@@ -1,14 +1,29 @@
 import { Coord } from "../../types/CommunType";
+import { move } from "../../utils/helper";
 import { GameObject } from "./GameObject";
 
 export class Bullet extends GameObject {
-  constructor(position: Coord, velocity: Coord) {
+  constructor(position: Coord, degree: number, speed: number) {
     super();
     this.position = position;
-    this.velocity = velocity;
+    this.speed = speed;
+    this.degree = degree;
   }
+  //function to go increment until the trajectory is done
   update(): void {
-    this.position.x = this.position.x + this.velocity.x;
-    this.position.y = this.position.y + this.velocity.y;
+    this.position = move(this.position, this.degree, this.speed);
+    if (
+      this.position.x > 550 ||
+      this.position.y > 550 ||
+      this.position.x < -5 ||
+      this.position.y < -5
+    ) {
+      this.out = true;
+    } else {
+      this.out = false;
+    }
+  }
+  destroy(bullets: Bullet[]) {
+    bullets.shift();
   }
 }
