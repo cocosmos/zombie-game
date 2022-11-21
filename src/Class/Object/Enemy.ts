2071a6f186610c52c3accea6ee699d6e8bba32b2
Cocosmos <mipam.guillot@crea-inseec.com>
2022-11-21 22:16:09 +0100

    ✅ Make a random spawn of enemies

--- src/Class/Object/Enemy.ts
+++ src/Class/Object/Enemy.ts
@@ -1,16 +1,19 @@
 import { Coord, Type } from "../../types/CommunType";
 import { calculateAngle, degreeToRadian } from "../../utils/helper";
+import { getRandomArbitrary, getRandomFloat } from "../../utils/random";
+import { gameEvent } from "../GameEventDom";
 import { GameObject } from "./GameObject";
 export class Enemy extends GameObject {
   health: number = 1;
   damage: number = 1;
   level: number = 1;
-  constructor(position: Coord) {
+  constructor(level: number) {
     super();
-    this.position = position;
-    this.speed = 1;
+    this.position = this.setPosition();
+    this.speed = level; // same speed as level
     this.degree = 0;
     this.size = { w: 100, h: 100 };
+    this.level = level;
   }
 
   move(object: Coord) {
@@ -18,6 +21,51 @@ export class Enemy extends GameObject {
     const radian = degreeToRadian(this.degree);
     this.position.x = this.position.x - this.speed * Math.cos(radian);
     this.position.y = this.position.y - this.speed * Math.sin(radian);
+  }
+  //random position
+  setPosition(): Coord {
+    const outXY = 100;
+    const outXYSmall = -5;
+    const height = gameEvent.gameSize.h;
+    const width = gameEvent.gameSize.w;
+    // ex : -100, nh + 100
+    const randomHeightGameSize = getRandomArbitrary(-outXY, height + outXY);
+    // ex : -100, nw + 100
+    const randomWidthGameSize = getRandomArbitrary(-outXY, width + outXY);
+    // ex : -100, -5
+    const randomSizeSmall = getRandomArbitrary(-outXY, outXYSmall);
+    // ex : nh, nh + 100
+    const randomHeightPlus = getRandomArbitrary(height, height + outXY);
+    // ex : nw, nw + 100
+    const randomWidthPlus = getRandomArbitrary(width, width + outXY);
+    let x = 0;
+    let y = 0;
+
+    switch (getRandomFloat(0, 3, 0)) {
+      case 0:
+        x = randomWidthGameSize;
+        y = randomSizeSmall;
+        break;
+      case 1:
+        x = randomWidthGameSize;
+        y = randomHeightPlus;
+
+        break;
+      case 2:
+        x = randomSizeSmall;
+        y = randomHeightGameSize;
+        break;
+      case 3:
+        x = randomWidthPlus;
+        y = randomHeightGameSize;
+        break;
+    }
+
+    return { x: x, y: y };
+  }
+
+  setSpeed(speed: number): void {
+    this.speed = speed;
   }
 
   getHealth(): number {
